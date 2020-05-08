c86ca8f76058fa304ee1f649e928be46bb1fb5e3
Ernie Nettles <ernienettles@gmail.com>
2020-05-08 12:39:18 -0400

    Tests are done and passing

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -26,10 +26,11 @@ const CheckoutForm = (props) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <h2>Checkout Form</h2>
+        <h2 data-testid='header' >Checkout Form</h2>
         <label>
           First Name:
           <input
+            data-testid='firstname'
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
@@ -38,6 +39,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
+            data-testid='lastname'
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
@@ -46,6 +48,7 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
+            data-testid='address'
             name="address"
             value={values.address}
             onChange={handleChanges}
@@ -53,17 +56,29 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input
+            data-testid='city'
+            name="city" 
+            value={values.city} 
+            onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input
+            data-testid='state' 
+            name="state" 
+            value={values.state} 
+            onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input
+          data-testid='zip' 
+          name="zip" 
+          value={values.zip} 
+          onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid='button'>Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { findByTestId } = render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = findByTestId(/header/i);
+
+    expect(header).toBeTruthy();
+});
+
+test("form shows success message on submit with form details", () => {
+
+    const { findByTestId, getByTestId } = render(<CheckoutForm />)
+
+    const firstNameInput = getByTestId(/firstname/i);
+    const lastNameInput = getByTestId(/lastname/i);
+    const addressInput = getByTestId(/address/i);
+    const cityInput = getByTestId(/city/i);
+    const stateInput = getByTestId(/state/i);
+    const button = getByTestId(/button/i);
+    const successMessage = findByTestId(/successmessage/i);
+
+    fireEvent.change(firstNameInput, { target: { value: 'Ernie' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Nettles' } });
+    fireEvent.change(addressInput, { target: { value: '123 6th St' } });
+    fireEvent.change(cityInput, { target: { value: 'Jacksonville' } });
+    fireEvent.change(stateInput, { target: { value: 'Florida' } });
+    fireEvent.click(button)
+
+    expect(firstNameInput).toBeInTheDocument();
+    expect(lastNameInput).toBeInTheDocument();
+    expect(addressInput).toBeInTheDocument();
+    expect(cityInput).toBeInTheDocument();
+    expect(stateInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(successMessage).toBeTruthy();
+
+
+});
